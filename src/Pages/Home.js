a2ee0a4ea641bcb01d36b3d5f6cf31385563f7c4
James Ramirez <jramire678@gmail.com>
2020-04-29 19:57:19 -0400

    Current work

---
+++ src/Pages/Home.js
@@ -0,0 +1,43 @@
+import React from "react"
+import "../App.css";
+import { Link } from "react-router-dom";
+
+
+
+
+
+function Home() {
+    return(
+       
+     <nav className="a:link">
+        <ul className="home">
+            <Link to="/Pizza">
+                <h1>Pizza</h1>
+            </Link> 
+            <Link to="/Sandwiches">
+                <h1>Sandwiches</h1>
+            </Link>    
+            
+            <Link to="/Pasta">
+                <h1>Pasta</h1>
+            </Link>
+            <Link to="/Sides">
+                <h1>Sides</h1>
+            </Link>
+            <Link to="/Drinks">
+                <h1>Drinks</h1>
+            </Link>
+            <Link to="/Extras">
+                 <h1>Extras</h1>
+            </Link>
+        
+              
+        </ul>
+      </nav>
+    
+  
+  
+)
+}
+
+export default Home;
